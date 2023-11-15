1194ae298827c8817d9cf9211eec8ba087f6dd35
jaeshim <jaeshim@gmarket.com>
2023-11-15 16:41:36 +0900

    20231115

--- java.algorithm.problem.solving.beginner/src/test/java/inflearn/java/algorithm/problem/solving/beginner/ch2/Peak.java
+++ java.algorithm.problem.solving.beginner/src/test/java/inflearn/java/algorithm/problem/solving/beginner/ch2/Peak.java
@@ -7,25 +7,25 @@ import java.util.Arrays;
  */
 public class Peak {
 
-  static int[] dx={0,1,0,-1};
-  static int[] dy={-1,0,1,0};
+  static int[] dx = {0, 1, 0, -1};
+  static int[] dy = {-1, 0, 1, 0};
 
-  static int solution(int n, int[][] arr){
+  static int solution(int n, int[][] arr) {
     int answer = 0;
 
-    int[][] copy = new int[n+1][n+1];
+    int[][] copy = new int[n][n];
 
-    for(int i=0;i<n;i++){
-      for(int j=0;j<n;j++){
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < n; j++) {
 
         boolean isBong = true;
-        for(int k=0;k<4;k++){
-          int nx = arr[i][j] + dx[k];
-          int ny = arr[i][j] + dy[k];
+        for (int k = 0; k < 4; k++) {
+          int nx = i + dx[k];
+          int ny = j + dy[k];
 
-          if(nx >= 0 && ny >=0){
-            if(arr[nx][ny] > arr[i][j]){
-              isBong=false;
+          if ((nx >= 0 && nx < n) && (ny >= 0 && ny < n)) {
+            if (arr[nx][ny] > arr[i][j]) {
+              isBong = false;
               break;
             }
           }
@@ -38,13 +38,13 @@ public class Peak {
       }
     }
 
-
     return answer;
   }
 
   public static void main(String[] args) {
-    int n =5;
-    int [][]arr={{5,3,7,2,3},{3,7,1,6,1},{7,2,5,3,4},{4,3,6,4,1},{8,7,3,5,2}};
+    int n = 5;
+    int[][] arr = {{5, 3, 7, 2, 3}, {3, 7, 1, 6, 1}, {7, 2, 5, 3, 4}, {4, 3, 6, 4, 1},
+        {8, 7, 3, 5, 2}};
 
     System.out.println(solution(n, arr));
   }
