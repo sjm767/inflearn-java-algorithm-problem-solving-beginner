13114bd97390bd615d0dd4b602b4b8b5fa8b2442
jaeshim <sjm767@naver.com>
2024-10-13 15:37:10 +0900

    정렬 알고리즘 로직 수정

--- java.algorithm.problem.solving.beginner/src/test/java/inflearn/java/algorithm/problem/solving/beginner/ch6/SelectionSort.java
+++ java.algorithm.problem.solving.beginner/src/test/java/inflearn/java/algorithm/problem/solving/beginner/ch6/SelectionSort.java
@@ -5,30 +5,29 @@ package inflearn.java.algorithm.problem.solving.beginner.ch6;
  */
 public class SelectionSort {
 
-  static int[] solution(int n, int[] arr) {
-
-    for(int i=0;i<n;i++){
-      int idx = i;
-      for(int j=i+1;j<n;j++){
-        if(arr[j] < arr[i]){
-          idx = j;
+    static int[] solution(int n, int[] arr) {
+        for (int i = 0; i < n; i++) {
+            int idx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[idx]) {
+                    idx = j;
+                }
+            }
+            int tmp = arr[i];
+            arr[i] = arr[idx];
+            arr[idx] = tmp;
         }
-      }
-      int tmp = arr[i];
-      arr[i] = arr[idx];
-      arr[idx] = tmp;
+
+        return arr;
     }
 
-    return arr;
-  }
+    public static void main(String[] args) {
+        int n = 6;
+        int[] arr = {13, 5, 11, 7, 23, 15};
 
-  public static void main(String[] args) {
-    int n = 6;
-    int[] arr = {13, 5, 11, 7, 23, 15};
-
-    int[] answer = solution(n, arr);
-    for (int a : answer) {
-      System.out.print(a+" ");
+        int[] answer = solution(n, arr);
+        for (int a : answer) {
+            System.out.print(a + " ");
+        }
     }
-  }
 }
